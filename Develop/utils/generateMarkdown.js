16805a96a81e0c2a381c5e3556e9f0494a331f2a
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2023-02-13 19:52:06 -0600

    worked on markdown file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,21 +1,92 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+const index = require('../index.js');
+
+function renderLicenseBadge(license) {
+  let badge = '';
+  if (license === 'MIT') {
+    badge = ''
+  } else if (license === 'Apache 2.0') {
+    badge = ''
+  } else if (license === 'GPL v3.0') {
+    badge = ''
+  }
+  return badge;
 }
 
-module.exports = generateMarkdown;
+function renderLicenseLink(license) {
+  let licenseLink = 'https://choosealicense.com/licenses/mit/';
+  if (license === 'MIT') {
+    licenseLink = ''
+  } else if (license === 'Apache 2.0') {
+    licenseLink = 'https://www.apache.org/licenses/LICENSE-2.0'
+  } else if (license === 'GPL v3.0') {
+    licenseLink = 'https://www.gnu.org/licenses/'
+  }
+  return licenseLink;
+}
 
+function renderLicenseSection(license) {
+  let licenseSection = ''
+  if (license === 'None') {
+    licenseSection - ''
+  } else {
+    licenseSection =
+      'License: $(license'
+  }
+  return licenseSection;
+}
+function generateMarkdown(answer) {
+
+  return'
+# ${answer.title}
+
+## ${renderLicenseSection(answer.license)} ${renderLicenseBadge(answer.license)}
+### ${renderLicenseLink(answer.license)}
+
+## Table of Contents:
+### = [License](#license)
+### = [Installation](#installation)
+### = [Usage](#usage)
+### = [Contributors](#contributors)
+### = [Questions](#questions)
+
+## Installation:
+### You must install the following for this app to function:
+### $(answer.installation)
+
+## Usage:
+### $(answer.usage)
+
+## Contributors:
+### $(answer.contributions)
+
+## Tests:
+### Run the following commands in your terminal to test the app:
+### ${answer.test}
+
+## Questions:
+### If you have any questions, you may contact me at either
+### GitHUb: https:github.com/${answer.askMe}
+### or
+### Email: ${answer.email}
+';
+}
+
+
+module.exports = generateMarkdown;
--- Develop/utils/index.js
+++ Develop/utils/index.js
@@ -1,25 +1,171 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
-init();
 
-const fs =require('fs');
+
+const fs = require('fs');
 const inquirer = require('inquire');
 const generateMarkdown = require('./generateMarkdown');
 
 const questions = [
     {
-        type 'input',
+        type: 'input',
         name: 'title',
-message: 'What is the title of your project?',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license does your project use?',
+        choices: ['None', 'Apache 2.0', 'MIT', 'GPL v 3.0'],
+        validate: LicenseInput = () => {
+            if (LicenseInput) {
+                return true;
+            } else {
+                console.log('Please select one fo the four options')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a project description',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Provide a project description')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What steps are needed to install your project?',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please provide installation steps')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: ('What is the use of your project'),
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Provide a use for your project')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'What guidelines must others follow in order to contribute?',
+        validate: contributionsInput => {
+            if (contributionsInput) {
+                return true;
+            } else {
+                console.log('Please enter contribution guidelines')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you test this project?',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('Provide a project description')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'askMe',
+        message: 'What is your Github username so others can reach your for questions?',
+        validate: askMeInput => {
+            if (askMeInput) {
+                return true;
+            } else {
+                console.log('Please provide your username so others can reach out to you with questions')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email so there is another way to be reached for questions?',
+        validate: emailInput => {
+            if (askMeInput) {
+                return true;
+            } else {
+                console.log('Please provide your username so others can reach out to you with questions')
+                return false;
+            }
+        }
+    }
+
+];
+
+//function to Write README file
+const writeToFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./generateREADME.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: ture
+            });
+        });
+    });
+};
+
+//to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then(function (answer) {
+            console.log(answer);
+            var fileContent = generateMarkdwon(answer);
+            writeToFile(fileContent)
+        });
 }
-]
+
+//to call to initialize app
+init();
+
+//export it
+module.exports = questions;
+

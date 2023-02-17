5cd1366a5c0fc9fe58da2d13d86ecc33986c897e
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2023-02-16 21:34:50 -0600

    put final touches on markdown file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,22 +1,9 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-
-
-// TODO: Create a function to generate markdown for README
-
-
-
 const fs = require('fs');
 const inquirer = require('inquirer');
 const index = require('./index');
 
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   let badge = '';
   if (license === 'MIT') {
@@ -29,6 +16,8 @@ function renderLicenseBadge(license) {
   return badge;
 }
 
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
 function renderLicenseLink(license) {
   let licenseLink = 'https://choosealicense.com/licenses/mit/';
   if (license === 'MIT') {
@@ -41,6 +30,9 @@ function renderLicenseLink(license) {
   return licenseLink;
 }
 
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
 function renderLicenseSection(license) {
   let licenseSection = ''
   if (license === 'None') {
@@ -53,7 +45,7 @@ function renderLicenseSection(license) {
 }
 function generateMarkdown(answer) {
 
-  return`
+  return `
 # ${answer.title}
 
 ## ${renderLicenseSection(answer.license)} ${renderLicenseBadge(answer.license)}
@@ -78,15 +70,15 @@ function generateMarkdown(answer) {
 
 ## Tests:
 ### Run the following commands in your terminal to test the app:
-### ${ answer.test }
+### ${answer.test}
 
 ## Questions:
 ### If you have any questions, you may contact me at either
-### GitHUb: https: github.com / ${ answer.askMe }
+### GitHUb: https: github.com / ${answer.askMe}
 ### or
-### Email: ${ answer.email }
+### Email: ${answer.email}
 `;
 }
+// TODO: Create a function to generate markdown for README
+
 module.exports = generateMarkdown;
-// i went on line 85
-//
--- Develop/utils/generateREADME.md
+++ Develop/utils/generateREADME.md
@@ -1,8 +1,8 @@
 
-# g
+# README OR README NOT
 
-## License:  ![License](https://img.shields.io/github/license/judgedgeo/readme-or-readme-not?label=license&style=for-the-badge)
-### https://choosealicense.com/licenses/mit/
+## License:  ![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
+### https://www.apache.org/licenses/LICENSE-2.0
 
 ## Table of Contents:
 ### * [License](#license)
@@ -13,20 +13,20 @@
 
 ## Installation:
 ### You must install the following for this app to function:
-### asa
+### Follow the steps above
 
 ## Usage:
-### qewq
+### Generate a README file
 
 ## Contributors:
-### et
+### Jsut the follow the steps
 
 ## Tests:
 ### Run the following commands in your terminal to test the app:
-### b
+### Follow the steps and type away
 
 ## Questions:
 ### If you have any questions, you may contact me at either
-### GitHUb: https: github.com / czx
+### GitHUb: https: github.com / Judgedgeo
 ### or
-### Email: h
+### Email: gjordan1973@icloud.com
